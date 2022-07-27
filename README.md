7118a71584f87599a7b6486f4da90d1328d61e20
carraes <carraeshb@gmail.com>
2022-07-27 20:13:05 -0300

    Structure done, no style, check readme.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# pgtracker
+# Pg T
+
+Doing something while i learn about APIs and more about DOM manipulation.
+
+## Versions
+
+07/27 - v0.1 - Got the api and table writing from json working.
---
+++ app.js
@@ -0,0 +1,33 @@
+let tableBody = document.querySelector('#dataD');
+
+const getData = async () => {
+    try{
+        const pegaData = await axios.get('https://api-apollo.pegaxy.io/v1/pegas/owner/user/0x3482541038d7002f6E22B001526b6FAfa71074eD');
+        dataToTable(pegaData);
+    }catch(e){
+        console.log(e);
+    }
+}
+
+const dataToTable = (data) => {
+    for(let key in data.data){
+        let nTr = document.createElement('tr');   
+        tableBody.append(nTr);
+        drawRow(data.data[key]);
+    }
+}
+
+const drawRow = row =>{
+    drawCell(row.id);
+    drawCell(row.name);
+    drawCell(row.breedType);
+    drawCell(row.winRate);
+}
+
+const drawCell = (cell) =>{
+    let nTd = document.createElement('td');
+    nTd.innerText = cell;
+    tableBody.append(nTd);
+}
+
+getData();
